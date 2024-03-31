4ff0c29c8c52c910d276f138750e99b4e80d2552
suhpark <suhpark@gmarket.com>
2024-03-31 23:31:22 +0900

    수정

--- src/test/java/com/kidari/api/application/service/LectureServiceTest.java
+++ src/test/java/com/kidari/api/application/service/LectureServiceTest.java
@@ -1,27 +1,17 @@
 package com.kidari.api.application.service;
 
 import com.kidari.api.application.port.out.*;
-import net.bytebuddy.asm.Advice;
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
 import net.jqwik.api.ShrinkingMode;
 import net.jqwik.api.constraints.IntRange;
 import net.jqwik.api.constraints.Size;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.params.provider.Arguments.of;
 
 public class LectureServiceTest {
 
@@ -49,39 +39,5 @@ public class LectureServiceTest {
             assertThat(lectureService.isAvailable(now.plusDays(diff), now))
                     .isEqualTo(expected);
         }
-    }
-
-    @ParameterizedTest
-    @MethodSource("testCases")
-    void isAvailable_parameterized(Integer diff, Boolean expected) {
-
-        assertThat(lectureService.isAvailable(LocalDateTime.now().plusDays(diff), LocalDateTime.now()))
-                .isEqualTo(expected);
-    }
-
-    static Stream<Arguments> testCases() {
-        return Stream.of(
-                of(-10, false),
-                of(-9, false),
-                of(-8, false),
-                of(-7, false),
-                of(-6, false),
-                of(-5, false),
-                of(-4, false),
-                of(-3, false),
-                of(-2, false),
-                of(-1, true),
-                of(0, true),
-                of(1, true),
-                of(2, true),
-                of(3, true),
-                of(4, true),
-                of(5, true),
-                of(6, true),
-                of(7, true),
-                of(8, false),
-                of(9, false),
-                of(10, false)
-        );
     }
 }
